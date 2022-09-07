460e1ceb751c773b43837ed600c304712921be68
hants <hantsawilliams@gmail.com>
2022-09-07 12:25:25 -0400

    updates

--- transformation/pyScripts/p3_stonyBrookClean.py
+++ transformation/pyScripts/p3_stonyBrookClean.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 
 # load the data
-stonybrook = pd.read_csv('transformation/dataFiles/113243405_StonyBrookUniversityHospital_standardcharges.csv')
+stonybrook = pd.read_csv('transformation/dataFiles/raw/113243405_StonyBrookUniversityHospital_standardcharges.csv')
 
 ## looking at the data frame, it is currently in WIDE format, we want to make it STACKED format
 ## so first lets get a name of the columns 
@@ -13,3 +13,7 @@ valueVars = columnNames[8:] #these are the idential variables that we want to 's
 ## transofmring with the melt function from a wide dataframe to a stacked dataframe
 stonybrook_modified = stonybrook.melt(id_vars=idVars, value_vars=valueVars)
 
+print(stonybrook_modified.head(50))
+
+stonybrook_modified.to_csv('transformation/dataFiles/clean/113243405_StonyBrookUniversityHospital_standardcharges_clean.csv')
+stonybrook_modified.shape
